359916463682931c1436dc61876434e29142e342
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-10-30 09:18:12 -0300

    implemented logic to set and display player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState();
+  const enteredPlayerName = useRef();
+
+  function handleClick() {
+    setPlayerName(enteredPlayerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : "unknow entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={enteredPlayerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
